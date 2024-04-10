6acdcf2ceb66e952621d150330170770a1b5cc3a
xoxo <95187401+Thexoxo@users.noreply.github.com>
2024-04-10 23:53:34 +0200

    Add files via upload

---
+++ proxy-dll/madll1/dllmain.cpp
@@ -0,0 +1,19 @@
+// dllmain.cpp : Définit le point d'entrée de l'application DLL.
+#include "pch.h"
+#include <windows.h>
+#pragma comment (lib, "user32.lib")
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, L"Hello from DLL!", L"DLL Message", MB_OK | MB_ICONINFORMATION);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+    
